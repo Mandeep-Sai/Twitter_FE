ce3fe8b4ced9e0c202edd84f867f6d6299b584cd
Mandeep Sai <63707885+Mandeep-Sai@users.noreply.github.com>
2020-09-30 23:42:10 +0200

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -55,7 +55,7 @@ export class Login extends Component {
       {
         headers: new Headers({
           "content-type": "application/json",
-          "Access-Control-Allow-Origin": `${process.env.REACT_APP_BACKEND_CONNECTION_URL}`,
+          "Access-Control-Allow-Origin": "https://twitterfe.herokuapp.com",
         }),
         credentials: "include",
         method: "POST",
